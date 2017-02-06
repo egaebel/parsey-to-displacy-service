cf6d9c740f58b0df1f064e1dcff4ec6d7d65d680
Ethan Gaebel <egaebel@vt.edu>
2017-02-05 23:40:45 -0800

    Update parsey.sh path.

--- src/parsey.sh
+++ src/parsey.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 old_dir=$(pwd)
-#cd ../../../scrapy/scrapy-parsey-playground-2.7/lib/parsey-mcparse-face/models/syntaxnet/
-cd ../../../parsey-mcparseface/src/models/syntaxnet/
+cd ../../parsey-mcparseface/src/models/syntaxnet/
 echo Input 1: $1
 echo $1 | ./syntaxnet/demo.sh
 cd $old_dir
